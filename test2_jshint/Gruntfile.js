d80f4b42c73c430b06d8c04a45ad072c7adcab66
panduozhi <panduozhi@126.com>
2015-06-11 12:06:40 +0800

    ''

---
+++ test2_jshint/Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function(grunt){
+	grunt.loadNpmTasks("grunt-contrib-jshint");
+
+	grunt.initConfig({
+		jshint:{
+			options:{
+				curly: true,
+				eqeqeq: true
+			},
+			target1: ['Gruntfile.js', 'src/**/*.js']
+		}
+	});
+
+	grunt.registerTask("default", ['jshint']);
+};
